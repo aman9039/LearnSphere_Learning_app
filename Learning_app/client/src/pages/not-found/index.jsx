68cbedbbf594598d44b85e06cd4148ab8f6026a9
amangupta <amangupta9039093489@gmail.com>
2024-12-26 09:39:08 +0530

    add not-found page

---
+++ Learning_app/client/src/pages/not-found/index.jsx
@@ -0,0 +1,10 @@
+
+
+
+function NotFoundPage() {
+    return ( 
+        <div className="h-20 max-w-80 text-center align-middle text-3xl font-sans m-auto mt-40 bg-black text-white">This page doesn't exist</div>
+     );
+}
+
+export default NotFoundPage;
